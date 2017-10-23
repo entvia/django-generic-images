fb35c63cb210d3ed9ba8c599834a93c8a9433bab
Illya Konovalov <horbor@gmail.com>
2017-10-23 11:43:00 -0400

    Changed path to GenericTabularInline, GenericForeignKey for compatibility with new django versions

--- generic_images/admin.py
+++ generic_images/admin.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib import admin
-from django.contrib.contenttypes.generic import GenericTabularInline
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.utils.translation import ugettext_lazy as _
 
 from generic_images.models import AttachedImage
